be1b80d7859e2c49cbe7ae3393bc14e1cbe8be0e
Estuar Diaz <33070207+EstuarDiaz@users.noreply.github.com>
2019-03-11 17:00:28 -0600

    Funcionamiento completo

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,75 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		Scanner input = new Scanner(System.in);
-        /* Leer diccionario */
+		String texto = "";
+		//Scanner input = new Scanner(System.in);
+		
+        /* Leer archivos */
 		File file = new File("diccionario.txt");
         Scanner scanner = new Scanner(file);
-        System.out.println("Diccionario:");
-        int n = 0;
-        BSTNode diccionario = new BSTNode("middle","mitad");
-        while (scanner.hasNextLine()){
-            String line = scanner.nextLine();
-            String[] palabras = line.split(",");
-        	System.out.println("[" + Integer.toString(++n) + "]: " + line);
-        	diccionario.put(palabras[0],palabras[1]);
+        if(scanner.hasNextLine()) {
+        	/* Leer diccionario */
+        	String[] palabras = scanner.nextLine().split(",");
+        	BinaryTree<String> diccionario = new BinaryTree<String>(palabras[0].trim(),palabras[1].trim());
+        	while (scanner.hasNextLine()){
+	            palabras = scanner.nextLine().split(",");
+	        	diccionario.put(palabras[0].trim(),palabras[1].trim());
+	        }
+	        scanner.close();
+	        
+	        /* Leer texto */
+	        file = new File("texto.txt");
+	        scanner = new Scanner(file);
+	        while (scanner.hasNextLine()){
+	        	texto += scanner.nextLine() + "\n";
+	        }
+	        texto = texto.substring(0, texto.length() - 1);
+	        scanner.close();
+	        
+	        /* Limpiar texto*/
+	        texto = texto.replace(".", " . ");
+	        texto = texto.replace(",", " , ");
+	        texto = texto.replace("(", " ( ");
+	        texto = texto.replace(")", " ) ");
+	        
+	        /* Traducir */
+	        String[] words = texto.split(" ");
+	        String traduccion = "";
+	        for(int i = 0; i < words.length; i++) {
+	        	if(words[i] != null) {
+	        		if(words[i].chars().allMatch(Character::isLetter)) {
+	        			Object palabra = diccionario.get(words[i]);
+			        	if(palabra != null) {
+			        		traduccion += palabra + " ";
+			        	}
+			        	else {
+			        		traduccion += "*" + words[i] + "* ";
+			        	}
+		        	}
+	        		else {
+	        			traduccion += words[i];
+	        		}
+	        	}
+	        }
+	        
+	        /* Limpiar traduccion de regreso*/
+	        traduccion = traduccion.replace(" .", ".");
+	        traduccion = traduccion.replace(" ,", ",");
+	        traduccion = traduccion.replace(" (", "(");
+	        traduccion = traduccion.replace(" )", ")");
+	        
+	        /* Mostrar resultado*/
+	        System.out.println("Texto Original: \n" + texto);
+	        System.out.println("\nTraduccion: \n" + traduccion);
+	        System.out.println("\nDiccionario ordenado:");
+	        diccionario.InOrder();
         }
-        scanner.close();
-        /* Leer texto */
-        n = 0;
-        System.out.println("\nResultado:");
-        diccionario.printData();
+        else {
+        	System.out.println("El diccionario esta vacio.");
+        	scanner.close();
+        }
 	}
 }
